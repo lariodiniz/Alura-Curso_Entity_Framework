d6853c0b9cf578b07dd4cda12c166f51217e1a14
larioDiniz <lario.diniz@palo.com.br>
2017-10-16 13:05:11 -0200

    Aula 01

---
+++ LojaComEntity/LojaComEntity/EntidadesContext.cs
@@ -0,0 +1,23 @@
+﻿using LojaComEntity.Entidades;
+using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaComEntity
+{
+    public class EntidadesContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string stringConexao = ConfigurationManager.ConnectionStrings["LojaConnectionString"].ConnectionString;
+            optionsBuilder.UseSqlServer(stringConexao);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ LojaComEntity/LojaComEntity/Program.cs
@@ -0,0 +1,31 @@
+﻿using LojaComEntity.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaComEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Aula 01 */
+            EntidadesContext contexto = new EntidadesContext();
+
+            Usuario renan = new Usuario()
+            {
+                Nome = "Renan",
+                Senha = "123"
+            };
+            contexto.Usuarios.Add(renan);
+            contexto.SaveChanges();
+            contexto.Dispose();
+
+            Console.WriteLine("Salvou o usuário.");
+            Console.ReadLine();
+
+        }
+    }
+}
